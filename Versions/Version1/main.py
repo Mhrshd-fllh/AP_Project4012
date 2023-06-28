a14d983a4559e08058f0583169c04e0596077476
Mehrshad <fallah_mehrshad82@comp.iust.ac.ir>
2023-06-28 11:51:37 +0330

    Deleting Kivy Projects and Starting to code with QT5

--- Versions/Version1/main.py
+++ Versions/Version1/main.py
@@ -1,36 +1,6 @@
 '''
-This is First Version of the Project That It's just the front of Project
-And Just its Homepage
+Importing QT5 
+Coding for Interface
+
 '''
-
-import kivy
-from kivy.app import App
-from kivy.lang import Builder
-from kivy.uix.screenmanager import ScreenManager, Screen
-
-
-kv = Builder.load_file('Versions/Version1/my.kv')
-
-class HomePage(Screen):
-    LoggedIn = True
-
-class MenuPage(Screen):
-    pass
-class WindowManager(ScreenManager):
-    pass
-
-sm = WindowManager()
-
-screens = [HomePage(name= "HomePage"), MenuPage(name= "MenuPage")]
-for screen in screens:
-    sm.add_widget(screen)
-
-sm.current = "MenuPage"
-
-class MyApp(App):
-    def build(self):
-        return sm
-    
-
-if __name__ == '__main__':
-    MyApp().run()
+import pyQt5
