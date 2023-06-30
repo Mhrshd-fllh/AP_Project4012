d818ac0b2a957128fde5f4b5be7bea5f8671ffd6
Mehrshad <fallah_mehrshad82@comp.iust.ac.ir>
2023-06-30 11:31:34 +0330

    Few Edits On Some Python Files

--- Versions/Selenium/Version2.py
+++ Versions/Selenium/Version2.py
@@ -7,12 +7,15 @@ import time
 import requests
 from bs4 import BeautifulSoup
 from unidecode import unidecode
+from googletrans import Translator, constants
+
+translator = Translator()
 
 driver = webdriver.Chrome()
 driver.get('https://www.digikala.com/search/category-mobile-phone/product-list/')
 time.sleep(5)
-price = {}
 screen = driver.find_element(By.XPATH, '//html')
+f = open('Digikala_Mobile_Prices.txt', 'w')
 for i in range(1, 50):
    
     temp = unidecode(driver.find_element(By.XPATH, f'//*[@id="ProductListPagesWrapper"]/section[1]/div[2]/div[{i}]/a/div/article/div[2]/div[2]/div[4]/div[1]/div/span').text)
@@ -21,14 +24,12 @@ for i in range(1, 50):
     elif temp == 'nmwjwd':
         continue
     else:
-        temp_name = driver.find_element(By.XPATH, f'//*[@id="ProductListPagesWrapper"]/section[1]/div[2]/div[{i}]/a/div/article/div[2]/div[2]/div[2]/h3').text
-        price[temp_name]= temp
+        temp_name = translator.translate(driver.find_element(By.XPATH, f'//*[@id="ProductListPagesWrapper"]/section[1]/div[2]/div[{i}]/a/div/article/div[2]/div[2]/div[2]/h3').text)
+        f.write(f'{temp_name.text}: {temp}\n')
     if i % 4 == 0:
         screen.send_keys(Keys.SPACE)
         screen.send_keys(Keys.SPACE)
         screen.send_keys(Keys.SPACE)
         time.sleep(4)  
-
-print(price)
 
 
