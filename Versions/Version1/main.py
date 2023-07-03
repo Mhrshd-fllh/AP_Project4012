970598392106ffc63f7f2c6adc3beb0a4c6829d5
Mehrshad <fallah_mehrshad82@comp.iust.ac.ir>
2023-07-03 10:32:26 +0330

    Starting Main App

--- Versions/Version1/main.py
+++ Versions/Version1/main.py
@@ -1,4 +1,20 @@
-'''
-Importing QT5 
-Coding for Interface
-'''
+#RUNNN! Final Version
+
+
+#Here We Import What We Need For This Program 
+import sys
+from PyQt5 import QtCore, QtGui, QtWidgets
+from functools import partial
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+
+#Here We Run Our Program 
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
